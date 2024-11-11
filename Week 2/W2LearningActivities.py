f1d643c83b64abb32d4232f50952a9135baa86e8
murilolhernandes <murilo.luiz.hernandes@gmail.com>
2024-11-11 15:07:28 -0700

    finished learning activities

--- Week 2/W2LearningActivities.py
+++ Week 2/W2LearningActivities.py
@@ -242,3 +242,65 @@
 # # Start this program by
 # # calling the main function.
 # main()
+
+# # g is a global variable because it
+# # is defined outside of all functions.
+# g = 25
+# def main():
+#   # x is a local variable because
+#   # it is defined inside a function.
+#   x = 1
+
+# nShapes = 0
+# def square_area(length):
+#   area = length * length
+#   return area
+# def rectangle_area(width, length):
+#   area = width * length
+#   return area
+
+# # Example 3 -- Common Mistake
+# import math
+# def main():
+#   radius = float(input("Enter the radius of a circle: "))
+#   area = circle_area()
+#   print(f"area: {area:.1f}")
+# def circle_area():
+#   # Mistake! There is no variable named radius
+#   # defined inside this function, so the variable
+#   # radius cannot be used in this function.
+#   area = math.pi * radius * radius
+#   return area
+
+# main()
+
+# # Example 4
+# import math
+# def main():
+#   radius = float(input("Enter the radius of a circle: "))
+#   area = circle_area(radius)
+#   print(f"area: {area:.1f}")
+# def circle_area(radius):
+#   area = math.pi * radius * radius
+#   return area
+# main()
+
+# # Example 5
+# import math
+# def main():
+#   # Call the arc_length function with only one argument
+#   # even though the arc_length function has two parameters.
+#   len1 = arc_length(4.7)
+#   print(f"len1: {len1:.1f}")
+#   # Call the arc_length function again but
+#   # this time with two arguments.
+#   len2 = arc_length(4.7, 270)
+#   print(f"len2: {len2:.1f}")
+#   # Define a function with two parameter. The
+#   # second parameter has a default value of 360.
+# def arc_length(radius, degrees=360):
+#   """Compute and return the length of an arc of a circle"""
+#   circumference = 2 * math.pi * radius
+#   length = circumference * degrees / 360
+#   return length
+# main()
